1efe685fa6a84c52cc43fd44111794c04415d8eb
Saksham Joshi <saksham.joshi08052006@gmail.com>
2026-01-23 13:37:07 +0530

    Backend setup: Express server connected to MongoDB for donation module

---
+++ backend/models/Donation.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const donationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    transactionId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    paymentMethod: {
+        type: String,
+        default: "UPI-QR"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Donation", donationSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
 
 const donationRoutes = require("./routes/donation.routes");
 
@@ -16,6 +20,12 @@ app.use("/api/donations", donationRoutes);
 app.get("/", (req, res) => {
     res.send("Backend is running successfully 🚀");
 });
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("✅ MongoDB connected"))
+    .catch((err) => console.error("❌ MongoDB error:", err));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
